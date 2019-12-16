0c93b582692c0b0a2adf612571002bc867ca360e
lucashockley <lucashockley@users.noreply.github.com>
2019-12-16 08:46:03 +0000

    Added cell event listener

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 const boardDisplay = document.getElementById('board');
 
-for (let i = 0; i < 64; i++) {
-  let cell = document.createElement('div');
-  cell.className = 'cell';
-  boardDisplay.appendChild(cell);
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    let cell = document.createElement('div');
+    cell.className = 'cell';
+    cell.id = `${j}-${i}`;
+    cell.onclick = cellClick;
+    boardDisplay.appendChild(cell);
+  }
 }
 
 let board = [];
@@ -12,4 +16,9 @@ for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
     board[i][j] = 0;
   }
+}
+
+function cellClick() {
+  let x = event.target.id[0];
+  let y = event.target.id[2];
 }
