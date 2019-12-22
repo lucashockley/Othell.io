a4594b68d9ba7c1082d62303f48b7f6adb81fb58
lucashockley <lucashockley@users.noreply.github.com>
2019-12-22 23:52:08 +0000

    Refactored file structure

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,3 @@
-const boardDisplay = document.getElementById('board');
-
-const disk = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-disk.setAttribute('width', '60');
-disk.setAttribute('height', '60');
-disk.setAttribute('viewBox', '0 0 24 24');
-disk.setAttribute('stroke-width', '2');
-
-const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-circle.setAttribute('cx', '12');
-circle.setAttribute('cy', '12');
-circle.setAttribute('r', '9');
-
-disk.appendChild(circle);
-
-const darkDisk = disk.cloneNode(true);
-darkDisk.className.baseVal = 'disk-dark';
-
-const lightDisk = disk.cloneNode(true);
-lightDisk.className.baseVal = 'disk-light';
-
-let darkMove;
-let lightMove;
-
 const inBoard = (x, y) => x > -1 && x < 8 && y > -1 && y < 8;
 
 class Game {
@@ -47,18 +23,8 @@ class Game {
     this.lightPlayer = lightPlayerType;
 
     if (this.darkPlayer === 'user') {
-      darkMove = disk.cloneNode(true);
-      darkMove.firstChild.setAttribute('onclick', 'selectMove()')
-      darkMove.className.baseVal = 'move-dark';
-
       this.showValidMoves(-1);
-    }
-    if (this.lightPlayer === 'user') {
-      lightMove = disk.cloneNode(true);
-      lightMove.firstChild.setAttribute('onclick', 'selectMove()')
-      lightMove.className.baseVal = 'move-light';
-    }
-    if (this.darkPlayer === 'ai') {
+    } else {
       this.aiMove();
     }
 
@@ -269,42 +235,4 @@ class Game {
       }
     }
   }
-}
-
-const createCells = () => {
-  boardDisplay.innerHTML = '';
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      let cell = document.createElement('div');
-      cell.className = 'cell';
-      cell.id = `${i}-${j}`;
-      boardDisplay.appendChild(cell);
-    }
-  }
-}
-
-createCells();
-
-let game;
-
-const startNewGame = () => {
-  if (game) {
-    game.running = false;
-    createCells();
-  }
-
-  let settings = document.getElementById('settings');
-
-  let darkPlayerType = settings.children[0].lastElementChild.firstElementChild.checked ? 'ai' : 'user';
-  let lightPlayerType = !settings.children[1].lastElementChild.firstElementChild.checked ? 'ai' : 'user';
-  let animations = !settings.children[2].lastElementChild.firstElementChild.checked;
-
-  game = new Game(darkPlayerType, lightPlayerType, animations, 500);
-}
-
-const selectMove = () => {
-  let x = Number(event.path[2].id[0]);
-  let y = Number(event.path[2].id[2]);
-
-  game.userMove(x, y);
 }
